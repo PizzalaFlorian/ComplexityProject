f0534cfc3990fa8478574df088b58162a5e537c6
florian <florian.pizzala@gmail.com>
2016-11-02 13:56:36 +0100

    rework with pathnodes

--- AntOptimization/class/path.php
+++ AntOptimization/class/path.php
@@ -1,50 +1,17 @@
 <?php
 
+	require('pathNode.php');
+
 	class Path{
 		private $lenght;//longeur du chemin
-		private $nodes_pheromone[];//tableau des noeud du chemin, on y stoque la valeur du phéromone.
-		private $nodes_fourmis[];
-		private $function_evaporation;//functon d'évaporation
+		private $list_node[];//liste des noeud du chemin séparer d'une distance de 1;
 
-		public function __construct($lenght,$function_evaporation){
+		public function __construct($lenght){
 			$this->lenght = $lenght;
-			$this->function_evaporation = $function_evaporation;
-			$this->nodes_pheromone[] = new array();
-			$this->nodes_fourmis[] = new array();
-			for($j=0;$j<=$lenght;$j++){
-				$this->nodes_pheromone[j]=0;
-				$this->nodes_fourmis[j]=0;
+			$this->list_node = new array();
+			for($j=0;$j<$lenght;$j++){
+				$this->list_node[j] = new PathNode();
 			}
-		}
-
-		/* ---------------------------------------------------------------------------------------------
-		*Met en place la valeur de pheromone a un indice du chemin
-		*renvoie false si valeurs incorecte
-		*renvoie true si réussite
-		--------------------------------------------------------------------------------------------- */
-		public function set_pheromone($indice_node,$value){
-			if($indice_node < 0 || $indice_node >= $this->lenght){
-				return false;
-			}
-			$this->nodes_pheromone[$indice_node] = $value;
-			return true;
-		}
-
-		/* ---------------------------------------------------------------------------------------------
-		*Incremente de "value" la valeur de pheromone a un indice du chemin
-		*renvoie false si valeurs incorecte
-		*renvoie true si réussite
-		--------------------------------------------------------------------------------------------- */
-		public function increment_pheromone($indice_node,$value){
-			if($indice_node < 0 || $indice_node >= $this->lenght){
-				return false;
-			}
-			$this->nodes_pheromone[$indice_node] += $value;
-			return true;
-		}
-
-		public function fourmis_avancent(){
-			$l = $this->lenght;
 		}
 	}
 
