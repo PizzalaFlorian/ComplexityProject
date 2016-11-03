6de279e0e57486f42bcd1c934578e3399ddd2f0b
florian <florian.pizzala@gmail.com>
2016-11-03 11:45:19 +0100

    je sais pas lire un sujet

--- AntOptimization/class/system.php
+++ AntOptimization/class/system.php
@@ -7,11 +7,11 @@
 		private $path2;
 		private $tour;
 
-		public function __construct($lenght_path1,$lenght_path1,$tempsCollectFood,$taux_apparition_fourmis,$taux_eveporation){
+		public function __construct($lenght_path1,$lenght_path1,$taux_apparition_fourmis,$taux_eveporation){
 			$this->taux_apparition_fourmis = $taux_apparition_fourmis;
 			$this->tour = 0 ;
-			$this->path1 = new Path($lenght_path1,$tempsCollectFood,$taux_eveporation);
-			$this->path2 = new Path($lenght_path2,$tempsCollectFood,$taux_eveporation);
+			$this->path1 = new Path($lenght_path1,$taux_eveporation);
+			$this->path2 = new Path($lenght_path2,$taux_eveporation);
 		}
 	}
 
